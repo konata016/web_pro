4dc68afa149c811abd2b07af866722a235949511
Unknown <konata016@gmail.com>
2018-06-07 19:49:38 +0900

    新規ファイル

---
+++ proken/Assets/UIover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIover : MonoBehaviour
+{
+
+    public Image gr;
+
+    // Use this for initialization
+    void Start()
+    {
+        gr.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void touch()
+    {
+        gr.enabled = true;
+    }
+    public void not_touch()
+    {
+        gr.enabled = false;
+    }
+}
---
+++ proken/Assets/UItouch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UItouch : MonoBehaviour {
+
+    public GameObject box;
+    public float speed = 3.0f;
+
+    float roll = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, roll, 0), step);
+    }
+
+    public void touch()
+    {
+        roll += 90;
+    }
+}
